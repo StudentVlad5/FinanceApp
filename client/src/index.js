c04c08bb903b723d19c3f9b962b58cfefca9f24a
Vladyslav Popov <vlad_np@ukr.net>
2025-08-04 18:13:53 +0200

    renew vercel json

--- client/src/index.js
+++ client/src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { persistor, store } from './redux/store';
 import { App } from './App';
 import { GlobalStyle } from 'components/baseStyles/GlobalStyle';
-
+import { HashRouter } from 'react-router-dom';
 import AOS from 'aos';
 import { ProjectProvider } from './context/ProjectContext';
 // import { I18nextProvider } from 'react-i18next';
@@ -25,7 +25,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
             {/* <StatusProvider> */}
             {/* <I18nextProvider i18n={i18n}> */}
             <GlobalStyle />
-            <App />
+            <HashRouter>
+              <App />
+            </HashRouter>
             {/* </I18nextProvider> */}
             {/* </StatusProvider> */}
           </BrowserRouter>
