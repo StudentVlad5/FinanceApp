05ef98f80b792fec9a9aa1807b1b6a9d9c0cbcf0
Vladyslav Popov <vlad_np@ukr.net>
2025-08-04 18:27:34 +0200

    correct base url

--- client/src/helpers/constants.jsx
+++ client/src/helpers/constants.jsx
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://finance-app-rouge-chi.vercel.app/api';
+export const BASE_URL = 'https://finance-app-novosad.vercel.app/api';
 export const BASE_URL_IMG =
   'https://res.cloudinary.com/dfqhj2far/image/upload/v1706095075/bloomskill_event/';
 
