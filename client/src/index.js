17491bb1af6364b312babf1d28d549fa1a322727
Vladyslav Popov <vlad_np@ukr.net>
2025-08-04 18:39:53 +0200

    remove hash routher

--- client/src/index.js
+++ client/src/index.js
@@ -7,7 +7,6 @@ import { BrowserRouter } from 'react-router-dom';
 import { persistor, store } from './redux/store';
 import { App } from './App';
 import { GlobalStyle } from 'components/baseStyles/GlobalStyle';
-import { HashRouter } from 'react-router-dom';
 import AOS from 'aos';
 import { ProjectProvider } from './context/ProjectContext';
 // import { I18nextProvider } from 'react-i18next';
@@ -25,9 +24,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
             {/* <StatusProvider> */}
             {/* <I18nextProvider i18n={i18n}> */}
             <GlobalStyle />
-            <HashRouter>
-              <App />
-            </HashRouter>
+            <App />
             {/* </I18nextProvider> */}
             {/* </StatusProvider> */}
           </BrowserRouter>
