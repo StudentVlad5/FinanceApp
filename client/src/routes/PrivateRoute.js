d0ef90557465e288d7576128f41c685f51b855b9
Vladyslav Popov <vlad_np@ukr.net>
2025-08-04 11:08:46 +0200

    renew vercel json

--- client/src/routes/PrivateRoute.js
+++ client/src/routes/PrivateRoute.js
@@ -1,9 +1,9 @@
-import { Navigate } from "react-router-dom";
-import PropTypes from "prop-types";
-import { useSelector } from "react-redux";
-import { selectIsLoggedIn, selectIsRefreshing } from "../redux/auth/selectors";
+import { Navigate } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { useSelector } from 'react-redux';
+import { selectIsLoggedIn, selectIsRefreshing } from '../redux/auth/selectors';
 
-export const PrivateRoute = ({ component: Component, redirectTo = "/" }) => {
+export const PrivateRoute = ({ component: Component, redirectTo = '/' }) => {
   const isLoggedIn = useSelector(selectIsLoggedIn);
   const isRefreshing = useSelector(selectIsRefreshing);
 
