c3f6ec284ab41d0e03f2e1bfc5c70fca69f7681c
Vladislav Popov <vlad_np@ukr.net>
2025-11-02 08:51:15 +0100

    add getReportsContragentsExpenses to report

--- controllers/reports/index.js
+++ controllers/reports/index.js
@@ -1,5 +1,7 @@
 const getReportsContragentsIncome = require('./getReportsContragentsIncome');
+const getReportsContragentsExpenses = require('./getReportsContragentsExpenses');
 
 module.exports = {
   getReportsContragentsIncome,
+  getReportsContragentsExpenses,
 };
--- routes/reports.js
+++ routes/reports.js
@@ -3,5 +3,6 @@ const router = Router();
 const { reports: ctrl } = require('../controllers');
 
 router.get('/get_contragents_income', ctrl.getReportsContragentsIncome);
+router.get('/get_contragents_expenses', ctrl.getReportsContragentsExpenses);
 
 module.exports = routerReports = router;
